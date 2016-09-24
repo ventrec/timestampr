17f2d30af8025f9f5975aaee412611c57cd0f6c0
Runar Jørgensen <ventrec@ventrec.org>
2016-09-24 19:13:50 +0200

    Checks for global vendor folder as well as local.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,9 @@ function includeIfExists($file)
     return file_exists($file) ? include $file : false;
 }
 
-if (!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php')) {
+if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php'))
+    and (!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))
+) {
     echo 'You must set up the project dependencies using `composer install`' . PHP_EOL;
 
     exit(1);
